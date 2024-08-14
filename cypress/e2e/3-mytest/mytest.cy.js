b67e28b34b3423d92c7449fbacfa98403118ff6d
kris <krisd3214@gmail.com>
2024-08-14 15:40:28 +0700

    fiv

--- cypress/e2e/3-mytest/mytest.cy.js
+++ cypress/e2e/3-mytest/mytest.cy.js
@@ -14,26 +14,4 @@ describe('App End-to-End Tests', () => {
     cy.get('[aria-label="Angular on twitter"]').should('have.attr', 'href', 'https://twitter.com/angular');
     cy.get('[aria-label="Angular on YouTube"]').should('have.attr', 'href', 'https://youtube.com/angular');
   });
-
-  it('should navigate to Learn Angular page on card click', () => {
-    cy.get('.card-container a')
-      .first()
-      .click();
-    cy.url().should('include', 'angular.io/tutorial');
-  });
-
-  it('should show terminal content', () => {
-    cy.get('.terminal').should('be.visible');
-    cy.get('.terminal pre').should('contain.text', 'Angular CLI');
-  });
-
-  it('should have responsive design', () => {
-    cy.viewport('iphone-6');
-    cy.get('.card-container').should('have.css', 'width', '100%');
-  });
-
-  it('should show modal on button click', () => {
-    cy.get('.card.card-small').click();
-    cy.get('.modal').should('be.visible');
-  });
 });
