ddf5f3e0f312dc179c954441bd9ba67ebf4e03a7
kris <krisd3214@gmail.com>
2024-08-15 11:30:24 +0700

    day18

--- cypress.config.ts
+++ cypress.config.ts
@@ -6,7 +6,9 @@ export default defineConfig({
       // implement node event listeners here
     },
     video: true,
+    screenshotOnRunFailure: true,
     videosFolder: 'cypress/videos',
+    screenshotsFolder: 'cypress/screenshots',
     baseUrl: 'http://localhost:4200',
     projectId: 'vij7ps' // เพิ่ม projectId ที่นี่
   },
